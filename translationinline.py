909e55c19c50bdee1bdfb0346de4ca43c2b4ae86
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-09 12:10:55 +0530

    Update translationinline.py

--- translationinline.py
+++ translationinline.py
@@ -4,12 +4,12 @@ from translationinline
 @Bot.on_message(filters.private & filters.media)
 async def _main(_, message):
     await message.reply_text(
-        "Where you want to Upload?",
+        "choose your Upload form ?",
         parse_mode="Markdown",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("DFile", callback_data="uptogofile"),
-                 InlineKeyboardButton("Video", callback_data="")]
+                [InlineKeyboardButton("file", callback_data="cb_string_video"),
+                 InlineKeyboardButton("video", callback_data="cb_string_video")]
             ]
         ),
